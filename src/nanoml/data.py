69e34ea66ce43552326edeaef1a00f53290f94fe
Shubhashis Roy Dipta <iamdipta@gmail.com>
2025-03-21 15:07:00 -0400

    updated dataset

--- src/nanoml/data.py
+++ src/nanoml/data.py
@@ -3,12 +3,11 @@ from datasets.config import DATASET_STATE_JSON_FILENAME
 from pathlib import Path
 
 
-def load_dataset_flexible(dataset_path, **kwargs):
-    """Load a dataset from a local path or from the Hugging Face Hub.
+def load_dataset_flexible(dataset_path: str, *args, **kwargs):
+    """Get the appropriate dataset loader based on the dataset path.
 
     Args:
         dataset_path (str): The path to the dataset.
-        kwargs (dict): Additional arguments to pass to the load_from_disk or load_dataset function.
 
     Raises:
         Exception: If the dataset is not found.
@@ -18,8 +17,8 @@ def load_dataset_flexible(dataset_path, **kwargs):
     """
     try:
         if Path(dataset_path, DATASET_STATE_JSON_FILENAME).exists():
-            return load_from_disk(dataset_path, **kwargs)
+            return load_from_disk(dataset_path, *args, **kwargs)
         else:
-            return load_dataset(dataset_path, **kwargs)
+            return load_dataset(dataset_path, *args, **kwargs)
     except Exception as e:
         raise e
