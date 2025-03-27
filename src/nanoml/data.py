a077b94fc242f6b1a4a0e3b5e9749b23a3e20f02
Shubhashis Roy Dipta <iamdipta@gmail.com>
2025-03-26 22:25:29 -0400

    added kwargs

--- src/nanoml/data.py
+++ src/nanoml/data.py
@@ -24,21 +24,22 @@ def load_dataset_flexible(dataset_path: str, *args, **kwargs):
         raise e
 
 
-def split_hf_dataset(dataset, val_size=0.1, test_size=0.1):
+def split_hf_dataset(dataset, val_size=0.1, test_size=0.1, **kwargs):
     """Split a Hugging Face dataset into train, validation, and test sets.
 
     Args:
-        dataset (datasets.Dataset): The dataset to split.
-        val_size (float | int, optional): The size of the validation set. Defaults to 0.1.
-        test_size (float | int, optional): The size of the test set. Defaults to 0.1.
+        | dataset (datasets.Dataset): The dataset to split.
+        | val_size (float | int, optional): The size of the validation set. Defaults to 0.1.
+        | test_size (float | int, optional): The size of the test set. Defaults to 0.1.
+        | **kwargs: Additional keyword arguments to pass to the `train_test_split` method.
 
     Returns:
         datasets.DatasetDict: A dictionary containing the train, validation, and test sets.
     """
-    train_val = dataset.train_test_split(test_size=val_size)
+    train_val = dataset.train_test_split(test_size=val_size, **kwargs)
     val = train_val["test"]
 
-    train_test = train_val["train"].train_test_split(test_size=test_size)
+    train_test = train_val["train"].train_test_split(test_size=test_size, **kwargs)
     train = train_test["train"]
     test = train_test["test"]
 
